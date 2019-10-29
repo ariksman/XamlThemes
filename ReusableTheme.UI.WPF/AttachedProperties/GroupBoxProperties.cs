6e7c010ea2fac8a1b59faa95721fa38bd0866894
Antti Riksman <antti.riksman@gmail.com>
2019-10-29 20:10:08 +0200

    Create GroupBoxProperties.cs

---
+++ ReusableTheme.UI.WPF/AttachedProperties/GroupBoxProperties.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace ReusableTheme.UI.WPF.AttachedProperties
+{
+  public static class GroupBoxProperties
+  {
+    /// <summary>
+    /// Identifies the CornerRadius property.
+    /// </summary>
+    public static readonly DependencyProperty CornerRadiusProperty =
+      DependencyProperty.RegisterAttached(
+        "CornerRadius",
+        typeof(double),
+        typeof(GroupBoxProperties),
+        new FrameworkPropertyMetadata(0d));
+
+    /// <summary>
+    /// Gets the corner radius.
+    /// </summary>
+    public static double GetCornerRadius(DependencyObject obj)
+    {
+      return (double)obj.GetValue(CornerRadiusProperty);
+    }
+
+    /// <summary>
+    /// Sets the corner radius.
+    /// </summary>
+    public static void SetCornerRadius(DependencyObject obj, double value)
+    {
+      obj.SetValue(CornerRadiusProperty, value);
+    }
+  }
+}
